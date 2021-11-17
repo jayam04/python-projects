633a88af58fca6d6b9f0500d1ed2682bdc1c3042
Jym Patel <93824505+JymPatel@users.noreply.github.com>
2021-11-17 09:07:37 +0530

    Add files via upload

---
+++ PyPrograms/areas.py
@@ -0,0 +1,23 @@
+
+# what we are going to do
+print("We can find areas of different shapes from below")
+
+# shapes we have
+print("1. Triangle")
+print("2. Square")
+
+# take input
+sipt = input("For what you would like to find area?")
+
+# if 1 is sel
+if sipt == "1" :
+    print("coming soon")
+
+elif sipt == "2" :
+    # get value of sides
+    side = float(input("What is size of side?"))
+    area = side**2
+    print("Area of square is", area)
+
+else :
+    print("You have selected wrong option. Please select from integer that has some shape associated with it")
