7a91699e7eca1a9772aac225c640303fad16e08a
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-12-09 11:38:29 -0800

    Create box.py

---
+++ PyPrograms/box/box.py
@@ -0,0 +1,4 @@
+print(" ╔═══════════════════════════════╗ ")
+print(" ║   This Box is licenced with   ║ ")
+print(" ║      the GPL V3 Licence!      ║ ")
+print(" ╚═══════════════════════════════╝ ")
