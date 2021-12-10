d28f5839229a1d9fa12de2257f7731f08abe0b77
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-12-10 10:14:29 -0800

    Text and Comment changes

--- PyPrograms/raspberry-pi-detector/rpi-detector.py
+++ PyPrograms/raspberry-pi-detector/rpi-detector.py
@@ -1,18 +1,18 @@
 # Raspberry Pi Detector
 # By @bupboi1337
-# Note: This program is licenced under GPL V3.
-# editor names go here:
+# Note: This program is licenced under the GPL V3 licence, see LICENCE to see the licence.
+# Editor names go here:
 #
 
 import os
 print("Raspberry Pi Detector")
 print("This uses the GPL V3 Licence. Read the file LICENCE to see it.")
-a = "raspberrypi.local" # name of the Raspberry Pi Local Hostname
-b = os.system("ping -c 1 " + a) # Ping raspberrypi.local
+ip = "raspberrypi.local" # Name of the default Raspberry Pi Local Hostname
+ping = os.system("ping -c 1 " + a) # Ping raspberrypi.local
 print("Pinging local Raspberry Pi IP Address...")
 
 # check the response
-if b == 0: # If the ping succedded
-  print a, 'was pinged successfuly! There is a Raspberry Pi on your Network!'
+if ping == 0: # If the ping worked
+  print('RPi local address ping successful! There is a Raspberry Pi on your Network!')
 else: # If the ping failed
-  print a, 'was unable to be pinged! There is probably not a Raspberry Pi on your network.'
+  print('RPi local address ping failed! There is probably not a Raspberry Pi on your network.')
