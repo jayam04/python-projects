83048cafda7c005163b7f389064a2f76c4dff48a
Jym Patel <93824505+JymPatel@users.noreply.github.com>
2021-12-29 14:07:55 +0530

    Update contacts.py

--- PyPrograms/contacts/contacts.py
+++ PyPrograms/contacts/contacts.py
@@ -28,23 +28,30 @@ while loopvar < 1:
         a = int(a)
     except ValueError:
         print("please enter an integral value")
-        
+    
+    # option 1 is selected
     if a == 1:
+        # get a new contact
         fname.append(input("First Name: "))
         lname.append(input("Last Name: "))
         number.append(input("Phone Number: "))
         email.append(input("email: "))
         
+    # if option 2 is selected
     elif a == 2:
         i1 = 0
         i2 = len(fname)
+        # print all names
         while i1 < i2:
             print(fname[i1], lname[i1], number[i1], email[i1])
             i1 += 1
         
+    # if option 0 is selected
     elif a == 0:
         print("DO YOU REALLY WANT TO EXIT")
         print("YOUR DATA SAVED IN CONTACTS WILL BE RESETED")
+        
+        # conform & exit
         inpt = input("y/n? ")
         if inpt == 'y' or inpt == 'Y':
             loopvar += 1
