427ed965574bc73ad753cc26c9916b5cd5b1484f
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2022-05-02 13:47:33 -0700

    Added a little dev note

--- PyPrograms/box/box.py
+++ PyPrograms/box/box.py
@@ -1,3 +1,5 @@
+# Dev note: Becuase the box uses Extended ascii it takes up a lot of ram and breaks in a lot of online python interpreters.
+# I dont know how to optimize the ram usage but maybe somebody can figure out how to reduce the ram usage.
 # box
 # by @bupboi1337 on github or 13Internet_Man37 on twitch.
 # Editors names can go down here: 
