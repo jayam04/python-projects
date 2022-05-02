6659fc1c081f82a15b4948a7c354084273c8a299
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2022-05-02 14:03:09 -0700

    Added link to issue and a small message.
    the message is asking for someone to remove the dev note after the bug is fixed

--- PyPrograms/box/box.py
+++ PyPrograms/box/box.py
@@ -1,5 +1,7 @@
 # Dev note: Becuase the box uses Extended ascii it takes up a lot of ram and breaks in a lot of online python interpreters.
 # I dont know how to optimize the ram usage but maybe somebody can figure out how to reduce the ram usage.
+# I made a issue regarding this issue here: https://github.com/JymPatel/Python-FirstEdition/issues/49
+# Remove this comment and all the comments before this after the bug is fixed.
 # box
 # by @bupboi1337 on github or 13Internet_Man37 on twitch.
 # Editors names can go down here: 
