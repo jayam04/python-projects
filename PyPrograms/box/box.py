bef7e3a301adfda212bfca3cf4c67f91b904e027
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2022-05-02 13:36:16 -0700

    Removal of a joke
    I replaced the joke with a spot for editors to add their names to the code
    praise gaben

--- PyPrograms/box/box.py
+++ PyPrograms/box/box.py
@@ -1,6 +1,7 @@
 # box
 # by @bupboi1337 on github or 13Internet_Man37 on twitch.
-# praise gaben
+# Editors names can go down here: 
+#
 print(" ╔═══════════════════════════════╗ ")
 print(" ║   This Box is licenced with   ║ ")
 print(" ║      the GPL V3 Licence!      ║ ")
