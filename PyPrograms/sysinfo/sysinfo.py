f3d8a2bf4c9c7af1601e9fca855a66d5c422124d
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2022-05-02 13:34:59 -0700

    minor additions to the comments

--- PyPrograms/sysinfo/sysinfo.py
+++ PyPrograms/sysinfo/sysinfo.py
@@ -2,11 +2,11 @@
 # By @bupboi1337
 # This uses the GPL V3 licence btw
 
-import platform
+import platform # import the Platform module to be able to scan the users system
 
 print("="*40, "System Information by @bupboi1337", "="*40)
 uname = platform.uname()
-print(f"System: {uname.system}")
-print(f"Release: {uname.release}")
-print(f"Version: {uname.version}")
-print(f"Machine: {uname.machine}")
+print(f"System: {uname.system}") 
+print(f"Release: {uname.release}") 
+print(f"Version: {uname.version}") 
+print(f"Machine: {uname.machine}") 
