24e4b010ae879e248797d0f0f3ba374c65e0fd77
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2022-09-15 10:32:11 -0700

    fix typos + change comments

--- PyPrograms/tkinter/box/box.py
+++ PyPrograms/tkinter/box/box.py
@@ -1,7 +1,7 @@
 # This program uses the GPL V3 Licence. Read LICENCE to see it.
 # By @bupboi1337
-# Editors can put thier names down here: 
+# Editors can put their names down here: 
 # 
-import tkinter # Import Tkinter, the library used to make a GUI.
-tk = tkinter.Tk()
-tk.mainloop() # Initalize a GUI Window
+import tkinter # Import Tkinter
+tk = tkinter.Tk() # shorten Tkinter
+tk.mainloop() # Initialize a GUI Window
